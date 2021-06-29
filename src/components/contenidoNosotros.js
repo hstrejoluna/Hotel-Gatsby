ee1c6265f10f9cd71e3660b0608ca0e7b9dd1358
Héctor Sebastián Trejo Luna <trejolunatutoriales@gmail.com>
2021-06-29 15:25:51 -0500

    finalizando habitaciones

--- src/components/contenidoNosotros.js
+++ src/components/contenidoNosotros.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Image from 'gatsby-image';
 import styled from '@emotion/styled';
-import { css } from '@emotion/core';
+import { css } from '@emotion/react';
 
 const Contenido = styled.main`
     padding-top: 4rem;
@@ -24,7 +24,7 @@ const ContenidoNosotros = () => {
 
     const resultado = useStaticQuery(graphql`
     query {
-        allDatoCmsPagina(filter: { slug: { eq: "nosotros" } }) {
+        allDatoCmsPagina(filter: { slug: { eq: "Nosotros" } }) {
               nodes {
               titulo
               contenido
