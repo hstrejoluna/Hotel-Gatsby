e3610b6d244d5efb29a6509dc8dce9aee3abcfa2
Héctor Sebastián Trejo Luna <trejolunatutoriales@gmail.com>
2021-06-29 01:14:44 -0500

    add datocms plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,12 @@ module.exports = {
          icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-datocms',
+      options: {
+        apiToken: '6a58b186708c979480d5bd60e6ce99'
+      }
+    },
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
